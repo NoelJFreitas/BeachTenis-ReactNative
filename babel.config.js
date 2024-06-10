f23ea7c5f4a3c3248f3f4def998ad6dcfea4db6b
noel jose <noel@adderesolutions.com>
2024-06-10 15:07:28 -0300

    feat: install fonts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@domain': './src/domain',
+          '@brand': './src/brand',
+          '@api': './src/api',
+          '@types': './src/types',
+          '@utils': './src/utils',
+          '@infra': './src/infra',
+          '@services': './src/services',
+          '@test': './src/test',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
